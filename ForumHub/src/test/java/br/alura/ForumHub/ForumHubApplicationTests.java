fa03465f9625d2d3cfaa2426e822083ab5c07d29
Thiago de sena lima <thiagosena316@gmail.com>
2024-06-25 15:35:19 -0300

    criação do projeto spring

---
+++ ForumHub/src/test/java/br/alura/ForumHub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.alura.ForumHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
